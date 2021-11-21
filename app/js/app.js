27b6ee144488998c16672e350348e373e6892bdc
Yulian <kokomoko12345koko@gmail.com>
2021-11-21 14:09:20 +0200

    end

--- app/js/app.js
+++ app/js/app.js
@@ -2,88 +2,88 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
-	// const date = new Date();
+	const date = new Date();
 
-	// const renderCalendar = () => {
-	// 	date.setDate(1);
+	const renderCalendar = () => {
+		date.setDate(1);
 	
-	// 	const monthDays = document.querySelector(".days");
+		const monthDays = document.querySelector(".days");
 	
-	// 	const lastDay = new Date(
-	// 		date.getFullYear(),
-	// 		date.getMonth() + 1,
-	// 		0
-	// 	).getDate();
+		const lastDay = new Date(
+			date.getFullYear(),
+			date.getMonth() + 1,
+			0
+		).getDate();
 	
-	// 	const prevLastDay = new Date(
-	// 		date.getFullYear(),
-	// 		date.getMonth(),
-	// 		0
-	// 	).getDate();
+		const prevLastDay = new Date(
+			date.getFullYear(),
+			date.getMonth(),
+			0
+		).getDate();
 	
-	// 	const firstDayIndex = date.getDay();
+		const firstDayIndex = date.getDay();
 	
-	// 	const lastDayIndex = new Date(
-	// 		date.getFullYear(),
-	// 		date.getMonth() + 1,
-	// 		0
-	// 	).getDay();
+		const lastDayIndex = new Date(
+			date.getFullYear(),
+			date.getMonth() + 1,
+			0
+		).getDay();
 	
-	// 	const nextDays = 7 - lastDayIndex - 1;
+		const nextDays = 7 - lastDayIndex - 1;
 	
-	// 	const months = [
-	// 		"Январь",
-	// 		"Февраль",
-	// 		"Март",
-	// 		"Апрель",
-	// 		"Май",
-	// 		"Июнь",
-	// 		"Июль",
-	// 		"Август",
-	// 		"Сентябрь",
-	// 		"Октябрь",
-	// 		"Ноябрь",
-	// 		"Декабрь",
-	// 	];
+		const months = [
+			"Январь",
+			"Февраль",
+			"Март",
+			"Апрель",
+			"Май",
+			"Июнь",
+			"Июль",
+			"Август",
+			"Сентябрь",
+			"Октябрь",
+			"Ноябрь",
+			"Декабрь",
+		];
 	
-	// 	document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+		document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 	
-	// 	document.querySelector(".date p").innerHTML = new Date().toDateString();
+		document.querySelector(".date p").innerHTML = new Date().toDateString();
 	
-	// 	let days = "";
+		let days = "";
 	
-	// 	for (let x = firstDayIndex; x > 0; x--) {
-	// 		days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
-	// 	}
+		for (let x = firstDayIndex; x > 0; x--) {
+			days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+		}
 	
-	// 	for (let i = 1; i <= lastDay; i++) {
-	// 		if (
-	// 			i === new Date().getDate() &&
-	// 			date.getMonth() === new Date().getMonth()
-	// 		) {
-	// 			days += `<div class="today">${i}</div>`;
-	// 		} else {
-	// 			days += `<div>${i}</div>`;
-	// 		}
-	// 	}
+		for (let i = 1; i <= lastDay; i++) {
+			if (
+				i === new Date().getDate() &&
+				date.getMonth() === new Date().getMonth()
+			) {
+				days += `<div class="today">${i}</div>`;
+			} else {
+				days += `<div>${i}</div>`;
+			}
+		}
 	
-	// 	for (let j = 1; j <= nextDays; j++) {
-	// 		days += `<div class="next-date">${j}</div>`;
-	// 		monthDays.innerHTML = days;
-	// 	}
-	// };
+		for (let j = 1; j <= nextDays; j++) {
+			days += `<div class="next-date">${j}</div>`;
+			monthDays.innerHTML = days;
+		}
+	};
 	
-	// document.querySelector(".prev").addEventListener("click", () => {
-	// 	date.setMonth(date.getMonth() - 1);
-	// 	renderCalendar();
-	// });
+	document.querySelector(".prev").addEventListener("click", () => {
+		date.setMonth(date.getMonth() - 1);
+		renderCalendar();
+	});
 	
-	// document.querySelector(".next").addEventListener("click", () => {
-	// 	date.setMonth(date.getMonth() + 1);
-	// 	renderCalendar();
-	// });
+	document.querySelector(".next").addEventListener("click", () => {
+		date.setMonth(date.getMonth() + 1);
+		renderCalendar();
+	});
 	
-	// renderCalendar();
+	renderCalendar();
 
 		const accordions = (accordionSelector) => {
 			const	accordion = document.querySelectorAll(accordionSelector);
@@ -114,6 +114,28 @@ document.addEventListener("DOMContentLoaded", function() {
 		
 		};
 		accordions('.part__accordion');
+
+	//---------------block-none----------
+
+		const hamburgerr = (hamburgerButton, hamburgerButtonActive, hamburgerNav, hamburgerNavActive, hamburgerHeader, headerMenuActive) => {
+			const button = document.querySelectorAll(hamburgerButton),
+									nav = document.querySelector(hamburgerNav),
+									header = document.querySelector(hamburgerHeader);
+
+			if(button) {
+					if(nav) {
+
+							button.forEach(element => {
+									element.addEventListener('click', (e) => {
+											element.classList.toggle(hamburgerButtonActive);
+											nav.classList.toggle(hamburgerNavActive);
+											header.classList.toggle(headerMenuActive);
+									});
+							});
+					}
+			}
+	};
+	hamburgerr('.header__language', 'header__language--active', '.header__language_select', 'header__language_select--active', '.header', 'a');
 
 		//----------------------TABS-JS----------------------
 	if (document.documentElement.clientWidth < 992) {
